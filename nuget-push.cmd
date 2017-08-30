fd68ae786bd36bd1ffdabadae968dab3a837b2c0
Troy Willmot <yort@yortondotnet.com>
2017-08-31 09:03:56 +1200

    Update nuget-push cmd for new version.

--- nuget-push.cmd
+++ nuget-push.cmd
@@ -1,2 +1,2 @@
-.nuget\nuget push Yort.Ntp.Portable.1.0.11.nupkg -Source https://www.nuget.org/api/v2/package
+.nuget\nuget push Yort.Ntp.Portable.1.0.12.nupkg -Source https://www.nuget.org/api/v2/package
 pause
