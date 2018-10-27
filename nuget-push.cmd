97dc4c9447da90aee9eb150ec576eed087b15cc7
Troy Willmot <yort@yortondotnet.com>
2018-10-27 21:11:50 +1300

    Nuget related updates for new release.

--- nuget-push.cmd
+++ nuget-push.cmd
@@ -1,2 +1,2 @@
-.nuget\nuget push Yort.Ntp.Portable.1.0.14.nupkg -Source https://www.nuget.org/api/v2/package
+.nuget\nuget push Yort.Ntp.Portable.2.0.0.nupkg -Source https://www.nuget.org/api/v2/package
 pause
