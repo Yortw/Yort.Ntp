fa56580b5be79846a72faa0b7b5bd80308f82e31
Troy Willmot <6725891+Yortw@users.noreply.github.com>
2024-10-05 22:41:15 +1300

    Make net40 test project actually use net4.0, add 45 tests.

---
+++ src/Yort.Ntp.Net45.Tests/NtpClientTests.cs
@@ -0,0 +1,72 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Yort.Ntp.Net45.Tests
+{
+	[TestClass]
+	public class NtpClientTests
+	{
+		private System.Threading.AutoResetEvent _GotResultSignal;
+		private DateTime? _Result;
+
+		[TestMethod]
+		[TestCategory("NetworkRequiredTests")]
+		public void NtpClient_DefaultServer_GetsNonNullResponse()
+		{
+			_GotResultSignal = new System.Threading.AutoResetEvent(false);
+			var client = new Yort.Ntp.NtpClient();
+			try
+			{
+				client.TimeReceived += Client_TimeReceived;
+				client.ErrorOccurred += Client_ErrorOccurred;
+				client.BeginRequestTime();
+				_GotResultSignal.WaitOne(1000);
+				Assert.IsNotNull(_Result);
+			}
+			finally
+			{
+				client.TimeReceived -= this.Client_TimeReceived;
+				client.ErrorOccurred -= this.Client_ErrorOccurred;
+			}
+		}
+
+		[TestMethod]
+		[TestCategory("NetworkRequiredTests")]
+		public void NtpClient_DefaultServer_GetsValidResponsesOverMultipleRequests()
+		{
+			var ntpEpoch = new DateTime(1900, 01, 01, 0, 0, 0, DateTimeKind.Utc);
+			_GotResultSignal = new System.Threading.AutoResetEvent(false);
+			var client = new Yort.Ntp.NtpClient();
+			try
+			{
+				client.TimeReceived += Client_TimeReceived;
+				client.ErrorOccurred += Client_ErrorOccurred;
+				for (int cnt = 0; cnt < 60; cnt++)
+				{
+					client.BeginRequestTime();
+					_GotResultSignal.WaitOne(2000);
+					Assert.IsNotNull(_Result);
+					Assert.AreNotEqual(ntpEpoch, _Result);
+				}
+			}
+			finally
+			{
+				client.TimeReceived -= this.Client_TimeReceived;
+				client.ErrorOccurred -= this.Client_ErrorOccurred;
+			}
+		}
+
+		private void Client_ErrorOccurred(object sender, NtpNetworkErrorEventArgs e)
+		{
+			Assert.Fail(e.Exception.Message);
+			_GotResultSignal.Set();
+		}
+
+		private void Client_TimeReceived(object sender, NtpTimeReceivedEventArgs e)
+		{
+			System.Diagnostics.Debug.WriteLine(e.CurrentTime);
+			_Result = e.CurrentTime;
+			_GotResultSignal.Set();
+		}
+	}
+}
